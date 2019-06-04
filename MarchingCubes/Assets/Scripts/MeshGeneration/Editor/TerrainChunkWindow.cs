d04e134671f782ad432cb7f42ec5268f3371379b
Ignaz\Lukas <lukasneuhold2@gmail.com>
2019-06-04 21:37:11 +0200

    Started Main Terrain Gen Editor, wishing for better system to create areas and so on

---
+++ MarchingCubes/Assets/Scripts/MeshGeneration/Editor/TerrainChunkWindow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class TerrainChunkWindow : EditorWindow
+{
+
+    [MenuItem("Window/Cube March Terrain Editor")]
+    private static void OpenWindow()
+    {
+        TerrainChunkWindow window = GetWindow<TerrainChunkWindow>();
+
+        window.titleContent = new GUIContent("Terrain Chunk Editor");
+        window.Initialize();
+    }
+
+    public void Initialize()
+    {
+
+    }
+
+    public void OnGUI()
+    {
+        DrawWithLayout();
+    }
+
+    private void DrawWithLayout()
+    {
+        Rect r = EditorGUILayout.BeginVertical();
+        
+        EditorGUILayout.BeginVertical();
+    }
+
+    private void DrawChunkFormCreator(Rect baseRect)
+    {
+        EditorGUILayout.LabelField(new GUIContent("General Info"));
+    }
+
+    private void DrawGeneralInfoArea(Rect baseRect)
+    {
+        EditorGUILayout.LabelField(new GUIContent("Chunk Form Creator"));
+    }
+}
